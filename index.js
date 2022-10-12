55b99c8c489a0aa66a5974ea958b52526c048259
imai10310 <17039vbu@gmail.com>
2022-10-12 22:39:30 +0900

    アドイベントリスナー２つの案

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 'use strict';
 // mainListの情報を取得している
-const mainList = document.getElementById('mainList');
-// divを生成している
-const div = document.createElement('div');
-// 上記で生成したdivをmainListへ入れ込んでいる。
-mainList.append(div); //append追加
+// const mainList = document.getElementById('mainList');
+// // divを生成している
+// const div = document.createElement('div');
+// // 上記で生成したdivをmainListへ入れ込んでいる。
+// mainList.append(div); //append追加
 
 // ----------------------------------------------------------
-//元となるものを取得
-// 複製（調べる）
-// 複製したものを画面に追加する
-const hoge = document.getElementById('hoge');
-// 複製
-const hogeClone = document.createElement('div');
-mainList.append(hogeClone); //append追加
+// あん１
+// inputTaskの情報を取得し、クリックをしたらクローンが作成される関数を作成
+// const inputTask = (document.getElementById('inputTask').onclick = () => {
+//   inputTask.cloneNode(true);
+// });
+// mainList.append(inputTask); //append追加
+
+// あん２
+const inputTask = (document.getElementById('inputTask')
+// 取得したinputTaskの情報をもとに、クリックしたらクローンが作成されるというイベントを発生させている。
+inputTask.addEventListener('click', () => {
+  inputTask.cloneNode(true);
+});
+mainList.append(); //append追加
