890714e5aedcc577893ed03af329f38bd7120063
imai10310 <17039vbu@gmail.com>
2022-10-13 00:36:31 +0900

    アドイベントリスナーでインプットの複製ができた（複製もとでクリックしなきゃだめ）

--- index.js
+++ index.js
@@ -1,23 +1,36 @@
 'use strict';
-// mainListの情報を取得している
-// const mainList = document.getElementById('mainList');
-// // divを生成している
-// const div = document.createElement('div');
-// // 上記で生成したdivをmainListへ入れ込んでいる。
-// mainList.append(div); //append追加
+// mainListの情報を取得している;
+const mainList = document.getElementById('mainList');
+// divを生成している
+const div = document.createElement('div');
+// 上記で生成したdivをmainListへ入れ込んでいる。
+mainList.append(div); //append追加
 
 // ----------------------------------------------------------
-// あん１
+// ●アドイベントリスナーを使用し、クリック後にインプットが増える仕様にしたい！
+// 案１
 // inputTaskの情報を取得し、クリックをしたらクローンが作成される関数を作成
-// const inputTask = (document.getElementById('inputTask').onclick = () => {
-//   inputTask.cloneNode(true);
-// });
-// mainList.append(inputTask); //append追加
+// const inputTask = (document.getElementById('inputTask').addEventListener(click,()=>{
+//   inputTask.cloneNode(true)
+// }
+// inputTask.append(inputTask)
 
-// あん２
-const inputTask = (document.getElementById('inputTask')
-// 取得したinputTaskの情報をもとに、クリックしたらクローンが作成されるというイベントを発生させている。
+// 案２
+// inputTaskの情報を取得している。
+const inputTask = document.getElementById('inputTask');
+// クリックをしたらinputTask要素が複製される
 inputTask.addEventListener('click', () => {
-  inputTask.cloneNode(true);
+  const inputTaskClone = inputTask.cloneNode(true);
+  mainList.append(inputTaskClone); //append追加
 });
-mainList.append(); //append追加
+
+// 上記をmainListへ入れ込む
+
+// 案３
+// inputTaskの情報を取得している。
+// const inputTask = document.getElementById('inputTask');
+// // クリックをしたらinputTask要素が複製される
+// inputTask.onclick = () => {
+//   inputTask.cloneNode(true);
+// };
+// mainList.append(inputTask); //append追加
